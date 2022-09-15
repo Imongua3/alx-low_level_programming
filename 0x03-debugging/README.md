9f82651130480c0f380d9a14efe8c25f9ff14ddf
Imongua3 <imongua3@gmail.com>
2022-09-15 16:35:30 -0700

    My first debugging task

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging code errors using different techniques.
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+positive_or_negative()
+
+print_remaining_days()
+
+#endif /* MAIN_H */
