5d5162aba69ea0e92ddb3dc6f05921639605e36b
Imongua3 <imongua3@gmail.com>
2022-09-19 07:01:03 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This project deals with data structures using pointers and arrays. Arrays is a groupdata of the same type, pointers is used to manipulate memory location of data.
+
