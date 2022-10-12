b1c188fbc2b5c9e9b1a94b55b8f0a5742173199c
Imongua3 <imongua3@gmail.com>
2022-10-12 09:47:46 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,19 @@
-#include "function_pointers.h" 
-  
- /** 
- * print_name - prints a name 
- * @name:name to print 
- * @f: function which print name 
- */ 
- void print_name(char *name, void (*f)(char *))
- {
-       if ( name == NULL || f == NULL )
-                 return;
-         f(name);
- }
+#include "function_pointers.h"
+
+/**
+* array_iterator - executes a function on each element of an array
+* @array: array to be targeted
+* @size: size of array
+* @action: function to be executed
+*
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
